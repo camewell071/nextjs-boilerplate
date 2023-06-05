c024d3a6eebcb2b6f9bf2bbce8f47cb244dea269
SangNX <130561684+camewell071@users.noreply.github.com>
2023-06-05 15:37:24 +0700

    chore: build modules

---
+++ generators/plop-module.js
@@ -0,0 +1,24 @@
+module.exports = function (plop) {
+  plop.setGenerator('modules', {
+    description: 'application module logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'module name please',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/module/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: '../src/module/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs',
+      },
+    ],
+  })
+}
